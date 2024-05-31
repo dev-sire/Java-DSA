1d698b955cdba785d710656f40bb62eecff4d7d6
Aman Shahid <shahidamaan2019@gmail.com>
2024-05-31 23:21:32 +0500

    added BST Exercise

--- Data Structures/BinarySearchTree.java
+++ Data Structures/BinarySearchTree.java
@@ -1,56 +1,58 @@
-class TreeNode{
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
 
-    public TreeNode(int val){
+    public TreeNode(int val) {
         this.val = val;
         this.left = null;
         this.right = null;
     }
 }
 public class BinarySearchTree {
-    private TreeNode root;
+	private TreeNode root;
+	
+	public BinarySearchTree() {
+		this.root = null;
+	}
 
-    public BinarySearchTree(){
-        this.root = null;
-    }
-    public void insert(int val){
+	public boolean search(int val) {
+		TreeNode current = root;
+		
+		while(current != null) {
+			if (val == current.val) {
+				return true;
+			} else if (val < current.val) {
+				current = current.left;
+			} else {
+				current = current.right;
+			}
+		}
+		return false;
+	}
+	public void insert(int val) {
         TreeNode newNode = new TreeNode(val);
-        if(root == null){
+        if (root == null) {
             root = newNode;
             return;
         }
         TreeNode current = root;
-        while(current != null){
-            if(val < current.val){
-                if(current.left == null){
+        while (current != null) {
+            if (val < current.val) {
+                if (current.left == null) {
                     current.left = newNode;
                     return;
                 }
                 current = current.left;
-            } else if(val > current.val){
-                if(current.right == null){
+            } else if (val > current.val) {
+                if (current.right == null) {
                     current.right = newNode;
                     return;
                 }
                 current = current.right;
-            } else{
+            } else {
                 return;
             }
         }
-    }
-    public boolean search(int val){
-        TreeNode current = root;
-        while(current != null){
-            if(current.val == val){
-                return true;
-            } else if(current.val < val){
-                current = current.left;
-            } else{
-                current = current.right;
-            }
-        }
-        return false;
-    }
+	}	
 }
---
+++ Implementations/ExerciseQ5.java
@@ -0,0 +1,26 @@
+public class ExerciseQ5 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null){
+            if(val == root.val){
+                return root;
+            } else if(val < root.val){
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return null;
+    }
+}
