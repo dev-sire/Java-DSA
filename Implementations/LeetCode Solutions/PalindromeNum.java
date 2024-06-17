6994ae9c249aa7e9d961477ae56b03666d7c7bc7
Aman Shahid <shahidamaan2019@gmail.com>
2024-06-17 19:19:14 +0500

    daily problem

---
+++ Implementations/LeetCode Solutions/PalindromeNum.java
@@ -0,0 +1,15 @@
+public class PalindromeNum {
+    public static boolean isPalindrome(int x){
+        if(x < 0){
+            return false;
+        }
+        long reversed = 0;
+        int y = x;
+
+        while(y > 0){
+            reversed = reversed * 10 + y % 10;
+            y /= 10;
+        }
+        return reversed == x;
+    }
+}
