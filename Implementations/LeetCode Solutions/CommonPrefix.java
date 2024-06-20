bc7f23d19d6f05b036bd118167938db37d8dcd95
Aman Shahid <shahidamaan2019@gmail.com>
2024-06-20 16:33:29 +0500

    Common Prefix Problem

---
+++ Implementations/LeetCode Solutions/CommonPrefix.java
@@ -0,0 +1,13 @@
+public class CommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)
+            return "";
+        
+        for(int i=0; i<strs[0].length(); ++i)
+            for(int j=0; j<strs.length; ++j)
+                if(i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
+                    return strs[0].substring(0, i);
+
+        return strs[0];
+    }
+}
---
+++ Implementations/LeetCode Solutions/RepeatedSubstring.java
@@ -0,0 +1,6 @@
+public class RepeatedSubstring {
+    public boolean repeatedSubstringPattern(String s) {
+        final String ss = s + s;
+        return ss.substring(1, ss.length() - 1).contains(s);
+    }
+}
