a4b9c848ec97f00b7af2bbce6da2882131c48551
Aman Shahid <shahidamaan2019@gmail.com>
2024-06-22 21:44:30 +0500

    added regular expression matching

---
+++ Implementations/LeetCode Solutions/NumberOfNiceSubArrays.java
@@ -0,0 +1,26 @@
+public class NumberOfNiceSubArrays{
+    public int numberOfSubarrays(int[] nums, int k) {
+        return numberOfSubarraysAtMost(nums, k) - numberOfSubarraysAtMost(nums, k - 1);
+    }
+    private int numberOfSubarraysAtMost(int[] nums, int k){
+        int ans = 0;
+        int right = 0;
+        int left = 0;
+
+        while(right <= nums.length){
+            if(k >= 0){
+                ans += right - left;
+                if(right == nums.length)
+                    break;
+                if(nums[right] % 2 == 1)
+                    --k;
+                ++right;
+            } else {
+                if(nums[left]%2 == 1)
+                    ++k;
+                ++left;
+            }
+        }
+        return ans;
+    }
+}
