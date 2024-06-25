3361096b30bbbeaff5544e98174cf31c2181241c
Aman Shahid <shahidamaan2019@gmail.com>
2024-06-25 21:06:35 +0500

    added BSTToGreaterSum

---
+++ Implementations/LeetCode Solutions/BSTToGreaterSumTree.java
@@ -0,0 +1,25 @@
+public class BSTToGreaterSumTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    int val = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root.right != null) bstToGst(root.right);
+        
+        val = root.val = val + root.val;
+
+        if(root.left != null) bstToGst(root.left);
+
+        return root;
+    }
+    
+}
---
+++ Implementations/LeetCode Solutions/PowerOfTwo.java
@@ -0,0 +1,5 @@
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        return n >= 0 && Integer.bitCount(n) == 1; 
+    }
+}
