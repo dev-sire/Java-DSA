6df624e8920e8dc46f123aa41dd655da47c45c82
Aman Shahid <shahidamaan2019@gmail.com>
2024-07-06 16:03:30 +0500

    added pass the pillow

---
+++ Implementations/LeetCode Solutions/PassThePillow.java
@@ -0,0 +1,10 @@
+public class PassThePillow {
+    public int passThePillow(int n, int time) {
+        time %= (n - 1)*2;
+
+        if(time < n)
+            return 1 + time;
+
+        return n - (time - (n - 1));
+    }
+}
