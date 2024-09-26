b64a25c080475f4b81b229530d9a98c6633d869e
Aman Shahid <shahidamaan2019@gmail.com>
2024-09-26 21:27:51 +0500

    added My Calender I

---
+++ Leetcode Exercises/Medium/MyCalenderI.java
@@ -0,0 +1,18 @@
+package Medium;
+
+import java.util.TreeMap;
+
+class MyCalendar {
+    public boolean book(int start, int end) {
+        Integer low = timeline.lowerKey(end);
+    
+        if (low == null || timeline.get(low) <= start) {
+            timeline.put(start, end);
+            return true;
+        }
+    
+        return false;
+    }
+  
+    private TreeMap<Integer, Integer> timeline = new TreeMap<>();
+}
