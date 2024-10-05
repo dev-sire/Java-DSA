5107af5af9f1caccdb10ed0e78076353b11e6359
Aman Shahid <shahidamaan2019@gmail.com>
2024-10-05 16:54:21 +0500

    added permutation of a String

---
+++ Leetcode 75/Bit Manipulation/SingleNumber.java
@@ -0,0 +1,9 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(final int num : nums){
+            ans ^= num;
+        }
+        return ans;
+    }
+}
---
+++ Leetcode Exercises/Medium/ArrayPairsDivisiblebyK.java
@@ -0,0 +1,18 @@
+package Medium;
+
+public class ArrayPairsDivisiblebyK {
+    public boolean canArrange(int[] arr, int k) {
+        int[] count = new int[k];
+        for (int a : arr) {
+            a %= k;
+            ++count[a < 0 ? a + k : a];
+        }
+        if (count[0] % 2 != 0)
+            return false;
+        for (int i = 1; i <= k / 2; i++){
+            if (count[i] != count[k - i])
+                return false;
+        }
+        return true;
+    }
+}
